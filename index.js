134e8b82fb50de3ed450ef15b851ec3b4b31e906
Jonathan Sorci <scottsorci@gmail.com>
2019-03-04 20:36:05 -0600

    made it a little better lol

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
+//todo - fix the gulpfile thing, it only starts when typing gulp into the  console... not sure why lol 
+
 const express = require('express');
 const app = express();
 
-app.get('/', (req,res) =>{
-	res.send({hi: 'there'});
+//body parser time!
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
+
+//setting handlebars as view engine
+var exphbs  = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//routes
+
+app.get('/', function (req, res) {
+    res.render('home');
+});
+
+
+app.post('/login', function(request, response){
+    console.log(request.body);
+    var info = request.body
+    //lookup username and password here
+    if(info.username == 'hello' && info.password == 'world'){
+    	response.render('loggedin')
+    }
 });
 
 
